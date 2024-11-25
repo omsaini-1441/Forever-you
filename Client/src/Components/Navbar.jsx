e917a38f889591f46c3b50cd41b4c21d7ca176f1
omsaini-1441 <omsaini1441@gmail.com>
2024-11-25 17:19:15 +0530

    UI/Search Bar dynamic render

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {assets} from '../assets/assets'
 import { Link, NavLink } from 'react-router-dom'
 import { useState } from 'react'
+import { ShopContext } from '../Context/ShopContext'
 
 const Navbar = () => {
 
           const [visible, setVisible] = useState(false);
+          const {showSearch ,setShowSearch} = useContext(ShopContext);
 
   return (
     <div className='flex items-center justify-between py-5 font-medium '>
@@ -36,7 +38,7 @@ const Navbar = () => {
         </NavLink>
         </ul>
         <div className='flex items-center gap-6 '>
-            <img src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
+            <img onClick={()=> setShowSearch(!showSearch)}  src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
             <div className="group relative ">
              <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="" />
              <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4 '>
