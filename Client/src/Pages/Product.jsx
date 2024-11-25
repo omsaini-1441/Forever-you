39cb029b31440ae2ff5ea3f0e15bb11c60314d79
omsaini-1441 <omsaini1441@gmail.com>
2024-11-25 17:47:48 +0530

    UI/ Product page  data fetch and build-fix

--- Client/src/Pages/Product.jsx
+++ Client/src/Pages/Product.jsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { ShopContext } from '../Context/ShopContext';
+import { products } from '../assets/assets';
 
 const Product = () => {
+
+    const {productId} = useParams();
+    const {Products} = useContext(ShopContext);
+    const [productData, setProductData] = useState(false);
+
+    const fetchProductData = async () =>{
+
+      products.map((item)=>{
+        if(item._id === productId){
+          setProductData(item);
+          return null;
+        }
+
+      })
+    }
+
+    useEffect(()=>{
+      fetchProductData();
+    },[productId, products])
+
   return (
     <div>Product</div>
   )
