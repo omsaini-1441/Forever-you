4cd7b34ac396ef6af0dfd83f30b05e40103622e3
omsaini-1441 <omsaini1441@gmail.com>
2024-12-01 20:11:34 +0530

    UI/ Cart section complete

--- Client/src/Context/ShopContext.jsx
+++ Client/src/Context/ShopContext.jsx
@@ -1,5 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext();
 
@@ -10,8 +12,14 @@ const ShopContextProvider = (props) => {
     const [search,setSearch]= useState('');
     const [showSearch,setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
+    const navigate = useNavigate();
 
     const addToCart = async (itemId,size) => {
+
+        if(!size){
+            toast.error('Select Product Size');
+            return;
+        }
 
         let cartData = structuredClone(cartItems);
 
@@ -31,16 +39,51 @@ const ShopContextProvider = (props) => {
         setCartItems(cartData);
     }
 
-    useEffect(() => {
-      console.log(cartItems);
-      
+    const getCartCount =() =>{
+        let totalCount = 0;
+        for(const items in cartItems){
+            for(const item in cartItems[items]){
+                try {
+                    if (cartItems[items][item] > 0) {
+                        totalCount += cartItems[items][item];
+                    }
+                } catch (error) {
+                    
+                }
+            }
+        }
+        return totalCount;
+    }
 
-    }, [cartItems])
+    const updateQuantity = async (itemId,size,quantity) =>{
+            let cartData = structuredClone(cartItems);
+
+            cartData[itemId][size] = quantity;
+
+            setCartItems(cartData);
+    }
+
+    const getCartAmount =  ()=>{
+        let totalAmount = 0;
+        for(const items in cartItems){
+            let itemInfo =  products.find((product)=> product._id === items );
+            for(const item in cartItems[items]){
+                try {
+                    if (cartItems[items][item] > 0) {
+                        totalAmount += itemInfo.price * cartItems[items][item];
+                    }
+                } catch (error) {
+                    
+                }
+            }
+        }
+        return totalAmount;
+    }
     
 
     const value = {
             products, currency , delivery_fee , search , setSearch , showSearch , setShowSearch ,
-            cartItems , addToCart
+            cartItems , addToCart , getCartCount , updateQuantity , getCartAmount , navigate
                 }
 
     return (
