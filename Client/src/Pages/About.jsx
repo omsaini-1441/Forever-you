edbaf24ff03576b4de9132af3f073be1e799c577
omsaini-1441 <omsaini1441@gmail.com>
2024-12-02 13:24:21 +0530

    BUG/ NewsLetterBox file name issue fix

--- Client/src/Pages/About.jsx
+++ Client/src/Pages/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Title from "../Components/Title"
 import {assets} from '../assets/assets'
-import NewsLetterBox from '../Components/NewsletterBox'
+import NewsLetterBox from '../Components/NewsLetterBox'
 
 const About = () => {
   return (
